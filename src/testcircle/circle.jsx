e1d6a402f0014667e22c6e6eda5c65a41df3ced8
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2023-01-02 20:03:06 +0700

    test

--- src/testcircle/circle.jsx
+++ src/testcircle/circle.jsx
@@ -1,33 +1,46 @@
 import "./circle.css"
 import { Button } from "@chakra-ui/button"
 import { Box } from "@chakra-ui/layout"
+import { useState } from "react"
 
 export function Testcircle() {
-
+    const [animate, Setanimate] = useState(false);
 
     function Startanimate() {
         document.querySelectorAll('.circle').forEach(e => {
             e.classList.add('loaded');
-        })
-        // handle the resize for this demo
-        document.body.onresize = e => {
-            console.log('Demo Resized - Resetting the Transition')
-            // remove the class
-            document.querySelectorAll('.circle').forEach(e => {
-                e.classList.remove('loaded')
-            })
             setTimeout(e => {
-                console.log('Transition Start');
-                document.body.onload()
-            }, 3000)
-        }
+                document.querySelectorAll('.circle').forEach(e => {
+                    e.classList.remove('loaded')
+                })
+            }, 500)
+
+        })
     }
+
+    function Resetanimate() {
+        document.querySelectorAll('.circle').forEach(e => {
+            e.classList.remove('loaded')
+        })
+    }
+
     return (<>
         test
-        <Box>
+        <Box padding={"100px"}>
             <div class="circle first"></div>
             <div class="circle second"></div>
         </Box>
-        <Button onClick={Startanimate}>Start</Button>
+        <Button onClick={() => {
+            Startanimate()
+            // Setanimate(!animate);
+            // if (animate === true) {
+            //     Startanimate();
+            // }
+            // else {
+            //     Resetanimate();
+            // }
+        }
+
+        }>Start</Button>
     </>)
 }
